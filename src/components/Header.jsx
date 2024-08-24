dfbef75a256d0c4790b2ed844852d1baf694a344
Грона Оля <grona.hell@gmail.com>
2024-08-24 23:42:03 +0300

    add initial files

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import faceImage from "../assets/images/face2.jpg";
+
+const Header = () => {
+  return (
+    <header className="flex flex-wrap items-center justify-center">
+      <div className="w-[300px]">
+        <h1 className="text-[32px]">Hrona Olha</h1>
+        <h2 className="mt-9 text-2xl">Junior front-end developer</h2>
+      </div>
+      <img
+        src={faceImage}
+        alt="Olha's face"
+        className="rounded-full h-52 w-52"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+export const Navbar = () => {
+  return (
+    <nav className="container mt-10">
+      <ul className="flex justify-between">
+        <li>
+          <a>home</a>
+        </li>
+        <li>
+          <a>projects</a>
+        </li>
+        <li>
+          <a>github</a>
+        </li>
+        <li>
+          <a>linkedin</a>
+        </li>
+        <li>
+          <a>telegram</a>
+        </li>
+        <li>
+          <a>email</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
