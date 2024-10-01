4974e450bbba74b53657a9aef933ef0186e3b397
Грона Оля <grona.hell@gmail.com>
2024-10-01 23:22:39 +0300

    add techStack.js and import it to Skills section

--- src/assets/data/techStack.js
+++ src/assets/data/techStack.js
@@ -1,16 +1,37 @@
-// import React from "../images/react.png";
-// import NextJS from "../images/nextjs.png";
-// import Html from "../images/html.png";
-// import Css from "../images/css.png";
-// import Js from "../images/js.png";
-// import Ts from "../images/ts.png";
-// import Redux from "../images/redux.png";
-// import Tailwind from "../images/tailwind.png";
-// import Sass from "../images/sass.png";
-// import Github from "../images/github.png";
-// import Figma from "../images/figma.png";
-// import Git from "../images/git.png";
-// import Postman from "../images/postman.png";
-// import Webpack from "../images/webpack.png";
-// import Docker from "../images/docker.png";
-// import Mongodb from "../images/mongodb.png";
+import React from "../images/react.png";
+import NextJS from "../images/nextjs.png";
+import Html from "../images/html.png";
+import Css from "../images/css.png";
+import Js from "../images/js.png";
+import Ts from "../images/ts.png";
+import Redux from "../images/redux.png";
+import Tailwind from "../images/tailwind.png";
+import Sass from "../images/sass.png";
+import Github from "../images/github.png";
+import Figma from "../images/figma.png";
+import Git from "../images/git.png";
+import Postman from "../images/postman.png";
+import Webpack from "../images/webpack.png";
+import Docker from "../images/docker.png";
+import Mongodb from "../images/mongodb.png";
+
+const techStackIcons = [
+  { id: 1, src: React, alt: "React Icon" },
+  { id: 2, src: NextJS, alt: "NextJS Icon" },
+  { id: 3, src: Html, alt: "HTML Icon" },
+  { id: 4, src: Css, alt: "CSS Icon" },
+  { id: 5, src: Js, alt: "JS Icon" },
+  { id: 6, src: Ts, alt: "TS Icon" },
+  { id: 7, src: Redux, alt: "Redux Icon" },
+  { id: 8, src: Tailwind, alt: "Tailwind Icon" },
+  { id: 9, src: Sass, alt: "SASS Icon" },
+  { id: 10, src: Github, alt: "Github Icon" },
+  { id: 11, src: Figma, alt: "Figma Icon" },
+  { id: 12, src: Git, alt: "Git Icon" },
+  { id: 13, src: Postman, alt: "Postman Icon" },
+  { id: 14, src: Webpack, alt: "Webpack Icon" },
+  { id: 15, src: Docker, alt: "Docker Icon" },
+  { id: 16, src: Mongodb, alt: "Mongodb Icon" },
+];
+
+export default techStackIcons;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,18 @@
 // import React from 'react'
+import techStackIcons from "../assets/data/techStack";
 
 const Skills = () => {
-  return <section aria-label="Tech Stack">Skills</section>;
+  return (
+    <section aria-label="Tech Stack">
+      <ul>
+        {techStackIcons.map((techStackIcon) => (
+          <li key={techStackIcon.id}>
+            <img src={techStackIcon.src} alt={techStackIcon.alt} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Skills;
